390b5b93663685e26bc049f97eee0146f8f2cdee
aoopa <45785074+aoopa@users.noreply.github.com>
2019-02-25 13:34:10 +0700

    Create loading.sh

---
+++ loading.sh
@@ -0,0 +1,321 @@
+#!/bin/bash
+# Aoo Loading wrapper # Version 1.0.0.1
+# Script Developed by Apivat Pattana-Anurak
+# SysAdmin & Programmer Form #Bangkok #Thailand 
+# curl -O https://raw.githubusercontent.com/aoopa/linux-centos/master/loading.sh
+# sh loading.sh
+
+echo -ne ' A                         (10%)\r'
+sleep 1
+echo -ne ' Aoo                       (20%)\r'
+sleep 1
+echo -ne ' Aoo P                     (30%)\r'
+sleep 1
+echo -ne ' Aoo Pat                   (40%)\r'
+sleep 1
+echo -ne ' Aoo Patta                 (50%)\r'
+sleep 1
+echo -ne ' Aoo Pattana               (60%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-a             (70%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anu           (80%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anur          (90%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anurak       (100%)\r'
+echo -ne '\n'
+
+echo -ne ' A                         (10%)\r'
+sleep 1
+echo -ne ' Aoo                       (20%)\r'
+sleep 1
+echo -ne ' Aoo P                     (30%)\r'
+sleep 1
+echo -ne ' Aoo Pat                   (40%)\r'
+sleep 1
+echo -ne ' Aoo Patta                 (50%)\r'
+sleep 1
+echo -ne ' Aoo Pattana               (60%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-a             (70%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anu           (80%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anur          (90%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anurak       (100%)\r'
+echo -ne '\n'
+
+echo -ne ' A                         (10%)\r'
+sleep 1
+echo -ne ' Aoo                       (20%)\r'
+sleep 1
+echo -ne ' Aoo P                     (30%)\r'
+sleep 1
+echo -ne ' Aoo Pat                   (40%)\r'
+sleep 1
+echo -ne ' Aoo Patta                 (50%)\r'
+sleep 1
+echo -ne ' Aoo Pattana               (60%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-a             (70%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anu           (80%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anur          (90%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anurak       (100%)\r'
+echo -ne '\n'
+
+echo -ne ' A                         (10%)\r'
+sleep 1
+echo -ne ' Aoo                       (20%)\r'
+sleep 1
+echo -ne ' Aoo P                     (30%)\r'
+sleep 1
+echo -ne ' Aoo Pat                   (40%)\r'
+sleep 1
+echo -ne ' Aoo Patta                 (50%)\r'
+sleep 1
+echo -ne ' Aoo Pattana               (60%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-a             (70%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anu           (80%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anur          (90%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anurak       (100%)\r'
+echo -ne '\n'
+
+echo -ne ' A                         (10%)\r'
+sleep 1
+echo -ne ' Aoo                       (20%)\r'
+sleep 1
+echo -ne ' Aoo P                     (30%)\r'
+sleep 1
+echo -ne ' Aoo Pat                   (40%)\r'
+sleep 1
+echo -ne ' Aoo Patta                 (50%)\r'
+sleep 1
+echo -ne ' Aoo Pattana               (60%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-a             (70%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anu           (80%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anur          (90%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anurak       (100%)\r'
+echo -ne '\n'
+
+echo -ne ' A                         (10%)\r'
+sleep 1
+echo -ne ' Aoo                       (20%)\r'
+sleep 1
+echo -ne ' Aoo P                     (30%)\r'
+sleep 1
+echo -ne ' Aoo Pat                   (40%)\r'
+sleep 1
+echo -ne ' Aoo Patta                 (50%)\r'
+sleep 1
+echo -ne ' Aoo Pattana               (60%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-a             (70%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anu           (80%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anur          (90%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anurak       (100%)\r'
+echo -ne '\n'
+
+echo -ne ' A                         (10%)\r'
+sleep 1
+echo -ne ' Aoo                       (20%)\r'
+sleep 1
+echo -ne ' Aoo P                     (30%)\r'
+sleep 1
+echo -ne ' Aoo Pat                   (40%)\r'
+sleep 1
+echo -ne ' Aoo Patta                 (50%)\r'
+sleep 1
+echo -ne ' Aoo Pattana               (60%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-a             (70%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anu           (80%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anur          (90%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anurak       (100%)\r'
+echo -ne '\n'
+
+echo -ne ' A                         (10%)\r'
+sleep 1
+echo -ne ' Aoo                       (20%)\r'
+sleep 1
+echo -ne ' Aoo P                     (30%)\r'
+sleep 1
+echo -ne ' Aoo Pat                   (40%)\r'
+sleep 1
+echo -ne ' Aoo Patta                 (50%)\r'
+sleep 1
+echo -ne ' Aoo Pattana               (60%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-a             (70%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anu           (80%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anur          (90%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anurak       (100%)\r'
+echo -ne '\n'
+
+echo -ne ' A                         (10%)\r'
+sleep 1
+echo -ne ' Aoo                       (20%)\r'
+sleep 1
+echo -ne ' Aoo P                     (30%)\r'
+sleep 1
+echo -ne ' Aoo Pat                   (40%)\r'
+sleep 1
+echo -ne ' Aoo Patta                 (50%)\r'
+sleep 1
+echo -ne ' Aoo Pattana               (60%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-a             (70%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anu           (80%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anur          (90%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anurak       (100%)\r'
+echo -ne '\n'
+
+echo -ne ' A                         (10%)\r'
+sleep 1
+echo -ne ' Aoo                       (20%)\r'
+sleep 1
+echo -ne ' Aoo P                     (30%)\r'
+sleep 1
+echo -ne ' Aoo Pat                   (40%)\r'
+sleep 1
+echo -ne ' Aoo Patta                 (50%)\r'
+sleep 1
+echo -ne ' Aoo Pattana               (60%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-a             (70%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anu           (80%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anur          (90%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anurak       (100%)\r'
+echo -ne '\n'
+
+echo -ne ' A                         (10%)\r'
+sleep 1
+echo -ne ' Aoo                       (20%)\r'
+sleep 1
+echo -ne ' Aoo P                     (30%)\r'
+sleep 1
+echo -ne ' Aoo Pat                   (40%)\r'
+sleep 1
+echo -ne ' Aoo Patta                 (50%)\r'
+sleep 1
+echo -ne ' Aoo Pattana               (60%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-a             (70%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anu           (80%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anur          (90%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anurak       (100%)\r'
+echo -ne '\n'
+
+echo -ne ' A                         (10%)\r'
+sleep 1
+echo -ne ' Aoo                       (20%)\r'
+sleep 1
+echo -ne ' Aoo P                     (30%)\r'
+sleep 1
+echo -ne ' Aoo Pat                   (40%)\r'
+sleep 1
+echo -ne ' Aoo Patta                 (50%)\r'
+sleep 1
+echo -ne ' Aoo Pattana               (60%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-a             (70%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anu           (80%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anur          (90%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anurak       (100%)\r'
+echo -ne '\n'
+
+echo -ne ' A                         (10%)\r'
+sleep 1
+echo -ne ' Aoo                       (20%)\r'
+sleep 1
+echo -ne ' Aoo P                     (30%)\r'
+sleep 1
+echo -ne ' Aoo Pat                   (40%)\r'
+sleep 1
+echo -ne ' Aoo Patta                 (50%)\r'
+sleep 1
+echo -ne ' Aoo Pattana               (60%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-a             (70%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anu           (80%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anur          (90%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anurak       (100%)\r'
+echo -ne '\n'
+
+echo -ne ' A                         (10%)\r'
+sleep 1
+echo -ne ' Aoo                       (20%)\r'
+sleep 1
+echo -ne ' Aoo P                     (30%)\r'
+sleep 1
+echo -ne ' Aoo Pat                   (40%)\r'
+sleep 1
+echo -ne ' Aoo Patta                 (50%)\r'
+sleep 1
+echo -ne ' Aoo Pattana               (60%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-a             (70%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anu           (80%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anur          (90%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anurak       (100%)\r'
+echo -ne '\n'
+
+echo -ne ' A                         (10%)\r'
+sleep 1
+echo -ne ' Aoo                       (20%)\r'
+sleep 1
+echo -ne ' Aoo P                     (30%)\r'
+sleep 1
+echo -ne ' Aoo Pat                   (40%)\r'
+sleep 1
+echo -ne ' Aoo Patta                 (50%)\r'
+sleep 1
+echo -ne ' Aoo Pattana               (60%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-a             (70%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anu           (80%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anur          (90%)\r'
+sleep 1
+echo -ne ' Aoo Pattana-anurak       (100%)\r'
+echo -ne '\n'
